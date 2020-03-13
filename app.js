dbf52c0c101ce47a4cd9719fd5934b6caeb3d092
zacharyinserra <zachary@christiansteven.com>
2020-03-13 16:37:40 -0400

    Procfile and additon to skills

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server running on port 3000...");
 });
